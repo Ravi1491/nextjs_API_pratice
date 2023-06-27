54e751d1d1323030b66ef5251be0b1cff9c41cb7
Ravi Singh <ravi149185@gmail.com>
2023-06-27 15:05:59 +0530

    vercel analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,20 @@
 import Layout from '../comps/Layout'
 import '../styles/globals.css'
+import { Analytics } from '@vercel/analytics/react';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <html lang="en">
+    <head>
+      <title>Next.js</title>
+    </head>
+    <body>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+      <Analytics />
+    </body>
+  </html>
   )
 }
 
